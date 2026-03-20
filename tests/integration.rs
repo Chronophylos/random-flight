5c92edfcfdd158fe0e3a080ffed5f0c3500d3028
Nikolai Zimmermann <nikolai@chronophylos.com>
2026-03-20 15:42:17 +0100

    test: add integration tests for flight plan generation

---
+++ tests/integration.rs
@@ -0,0 +1,71 @@
+use std::time::Duration;
+
+use rand::SeedableRng;
+use rand::rngs::SmallRng;
+
+use random_flight::{
+    FlightPlanOptions, aircraft_by_name, generate_flight_plan_with_rng,
+};
+
+#[test]
+fn c172_one_hour_flight() {
+    let ac = aircraft_by_name("C172").unwrap();
+    let target = Duration::from_secs(3600);
+    let opts = FlightPlanOptions {
+        tolerance: Duration::from_secs(15 * 60),
+        ..Default::default()
+    };
+    let mut rng = SmallRng::seed_from_u64(123);
+    let fp = generate_flight_plan_with_rng(ac, target, Some(opts), &mut rng).unwrap();
+
+    let diff = fp.block_time.as_secs().abs_diff(target.as_secs());
+    assert!(diff <= 15 * 60, "block time off by {diff}s");
+}
+
+#[test]
+fn b738_four_hour_flight() {
+    let ac = aircraft_by_name("B738").unwrap();
+    let target = Duration::from_secs(4 * 3600);
+    let opts = FlightPlanOptions {
+        tolerance: Duration::from_secs(15 * 60),
+        ..Default::default()
+    };
+    let mut rng = SmallRng::seed_from_u64(456);
+    let fp = generate_flight_plan_with_rng(ac, target, Some(opts), &mut rng).unwrap();
+
+    let diff = fp.block_time.as_secs().abs_diff(target.as_secs());
+    assert!(diff <= 15 * 60, "block time off by {diff}s");
+}
+
+#[test]
+fn pinned_route_jfk_lax() {
+    let ac = aircraft_by_name("B738").unwrap();
+    let opts = FlightPlanOptions {
+        departure_icao: Some("KJFK".into()),
+        arrival_icao: Some("KLAX".into()),
+        ..Default::default()
+    };
+    let mut rng = SmallRng::seed_from_u64(789);
+    let fp = generate_flight_plan_with_rng(ac, Duration::from_secs(3600), Some(opts), &mut rng).unwrap();
+
+    assert_eq!(fp.departure.icao, "KJFK");
+    assert_eq!(fp.arrival.icao, "KLAX");
+    assert!(fp.distance_nm > 2000.0);
+}
+
+#[test]
+fn deterministic_with_same_seed() {
+    let ac = aircraft_by_name("A320").unwrap();
+    let target = Duration::from_secs(3 * 3600);
+    let opts1 = FlightPlanOptions::default();
+    let opts2 = FlightPlanOptions::default();
+
+    let mut rng1 = SmallRng::seed_from_u64(42);
+    let mut rng2 = SmallRng::seed_from_u64(42);
+
+    let fp1 = generate_flight_plan_with_rng(ac, target, Some(opts1), &mut rng1).unwrap();
+    let fp2 = generate_flight_plan_with_rng(ac, target, Some(opts2), &mut rng2).unwrap();
+
+    assert_eq!(fp1.departure.icao, fp2.departure.icao);
+    assert_eq!(fp1.arrival.icao, fp2.arrival.icao);
+}
